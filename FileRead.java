e2f2e41acc6741739e492958c9306a208685e9dd
abansal <abansal@ebay.com>
2015-08-24 07:22:47 -0700

    Changed the data structure to hold messages

--- FileRead.java
+++ FileRead.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * Created by abansal on 8/23/15.
@@ -11,14 +8,14 @@ import java.util.concurrent.TimeUnit;
 public class FileRead {
     public static void main(String[] args) {
         ExecutorService executor = Executors.newFixedThreadPool(3);
-        LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>();
+        ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<>();
 
         Runnable readFileTask = () -> {
             try {
                 BufferedReader br = new BufferedReader(new FileReader("/Users/abansal/dev/gitebay/IK/out/production/Test/test.txt"));
                 String currentLine;
                 while((currentLine = br.readLine()) != null){
-                    queue.put(currentLine);
+                    queue.add(currentLine);
                 }
             }catch (Exception e){ e.printStackTrace();}
         };
@@ -28,7 +25,7 @@ public class FileRead {
                 while(1>0) {
                     //Do heavy duty task
                     TimeUnit.SECONDS.sleep(2);
-                    System.out.println(queue.take());
+                    System.out.println(queue.remove());
                 }
             } catch (InterruptedException e) { e.printStackTrace();}
         };
